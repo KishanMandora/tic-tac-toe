024d19e7dd718e86ef442513d52f4cd296e140c9
Kishan Mandora <kishanmandora7@gmail.com>
2022-09-16 02:28:33 +0530

    feat: add restart feature

--- src/Components/Game.jsx
+++ src/Components/Game.jsx
@@ -10,8 +10,8 @@ function Game() {
   return (
     <section className="w-full h-screen flex flex-col justify-center items-center">
       <h2 className="text-4xl"> Tic Tak Toe </h2>
-      <div className="text-lg my-6"> {status} </div>
-      <div className="p-4 flex flex-col gap-4 border-4 border-solid rounded-lg">
+      <div className="text-lg mt-3"> {status} </div>
+      <div className="p-4 my-4 flex flex-col gap-4 border-4 border-solid rounded-lg">
         <div className="grid grid-cols-3 gap-4">
           {squares.map((square, index) => {
             return (
@@ -24,6 +24,13 @@ function Game() {
           })}
         </div>
       </div>
+
+      <button
+        className="btn btn-outline btn-error"
+        onClick={() => setSquares(Array(9).fill(null))}
+      >
+        Restart
+      </button>
     </section>
   );
 }
--- src/utils/calculateNextValue.js
+++
@@ -1,2 +0,0 @@
-export const calculateNextValue = (squares) =>
-  squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
